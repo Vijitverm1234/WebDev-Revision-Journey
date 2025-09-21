5bbf911983a193b20da88fa1eb2372e9539643a8
Vijit Verma <143703788+Vijitverm1234@users.noreply.github.com>
2025-09-21 16:15:45 +0000

    js interview

---
+++ JavaScript in/interview1.js
@@ -0,0 +1,28 @@
+// console.log(typeof undefined)
+
+
+// let arr=[]
+// arr.length=100
+// console.log(arr)
+
+
+
+
+
+
+// let func1=(num)=>{ return new Promise((resolve,reject)=>{
+//     if(num>10){
+//         console.log("more than 5")
+//     }
+//     else{
+//         console.log("less than 5")
+//     }
+// })}
+
+// func1(90).then((mess)=>{
+//     console.log(mess)
+// })
+
+// .catch((mess)=> console.log(mess))
+
+
